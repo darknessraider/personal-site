1c18a556a6aa372ba8290a100ff8f11ccf4e843b
woodyspivey <woodyspivey@gmail.com>
2025-06-02 01:44:22 -0400

    rearagnge some stuff and have a functional git ignore

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3001;
+const corsOptions = {
+	origin: ["http://localhost:5173"]
+};
 
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/api/hello', (_req, res) => {
 	res.json({ message: "Hello from Express + TypeScript!" });
+});
+
+app.get('/api', (_req, res) => {
+	res.json({ fruits: ["apple", "bannana", "raspberry"] })
 });
 
 app.listen(PORT, () => {
